14cb4cc5184ac639608e876ff38552f189fdf5c3
alluizadm-ai <a.lluizadm@gmail.com>
2026-02-08 20:33:57 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Escala Familiar 2026",
+  description: "App de escala familiar"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
