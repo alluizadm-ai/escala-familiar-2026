43122e208493a2a3350408e6922f9af6280c4e9f
alluizadm-ai <a.lluizadm@gmail.com>
2026-02-08 21:41:12 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
+import "./globals.css";
+
 export const metadata = {
   title: "Escala Familiar 2026",
   description: "App de escala familiar"
-}
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -10,5 +12,5 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
-  )
+  );
 }
